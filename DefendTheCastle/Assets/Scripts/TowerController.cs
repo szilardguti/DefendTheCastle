1532c3faddb8b70f6be7cb914e2597b19b50c4f6
szilardguti <szilardguti@gmail.com>
2022-04-26 23:44:38 +0200

    Added basic cannon shooting physics
    Used these forum posts to clump it together, but it works:
    -https://www.reddit.com/r/Unity3D/comments/do5ymo/how_to_predict_the_future_position_of_a_moving/
    - https://answers.unity.com/questions/38429/how-can-i-calculate-velocity-without-using-rigidbo.html

--- DefendTheCastle/Assets/Scripts/TowerController.cs
+++ DefendTheCastle/Assets/Scripts/TowerController.cs
@@ -6,6 +6,21 @@ public class TowerController : MonoBehaviour
 {
     public Transform target;
 
+    private Vector3 previousTargetPos;
+    private float targetVelocity;
+    private Vector3 targetVelocityVec;
+    private float targetTimeToFuturePos;
+
+    public GameObject cannonBallPrefab;
+
+
+    private void Start()
+    {
+        previousTargetPos = target.position;   
+    }
+
+
+
     // Update is called once per frame
     void Update()
     {
@@ -16,5 +31,20 @@ public class TowerController : MonoBehaviour
         //quaternion is rotation
         transform.Find("rotater").rotation = Quaternion.LookRotation(direction);
 
+
+        // calculate velocity of the target without rigidbody
+        targetVelocity = (target.position - previousTargetPos).magnitude * Time.deltaTime;
+        targetVelocityVec = (target.position - previousTargetPos) * Time.deltaTime;
+        targetTimeToFuturePos = Vector3.Distance(target.position, transform.position) / targetVelocity;
+
+        previousTargetPos = target.position;
+
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GameObject ballShot = Instantiate(cannonBallPrefab, transform.Find("rotater").Find("CannonBallSpawnPoint").position, transform.Find("rotater").rotation);
+
+            ballShot.GetComponent<Rigidbody>().AddForce( (target.position + (targetVelocityVec * 0.3f *targetTimeToFuturePos)) - transform.position, ForceMode.Impulse);
+        }
     }
 }
